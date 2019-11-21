067db4ebbe5179640ee84cf9f6ab26d524951a58
Parth Bhindi <45254591+parth2020@users.noreply.github.com>
2019-11-21 13:31:55 +0530

    Prog 14(1)

---
+++ Prog 14(1).cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, n;
+            Console.WriteLine("Enter The Number of Rows");
+            n = Convert.ToInt16(Console.ReadLine());
+            for (i = 1; i <= n; i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write(j);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
+
