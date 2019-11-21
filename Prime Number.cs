8025d8400c5f562213c82b3889245c2b204cf458
Parth Bhindi <45254591+parth2020@users.noreply.github.com>
2019-11-21 13:24:37 +0530

    Prog 12

---
+++ Prime Number.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s, f, i, j, cnt;
+            Console.WriteLine("Input the Starting Range:");
+            s = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Input the Ending Range:");
+            f = Convert.ToInt16(Console.ReadLine());
+
+            Console.Write("The prime numbers between {0} and {1} are : \n",s,f);
+
+            for (i = s; i <= f; i++)
+            {
+                cnt = 0;
+                for(j=2;j<=i/2;j++)
+                {
+                    if(i%j==0)
+                    {
+                        cnt = cnt +1;
+                        break;
+                    }
+
+                }
+                if(cnt==0 && i!= 1)
+                       Console.Write("{0} ",i);
+            }
+
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
