4f72c6aca63816f97c8cf352d27d36884bd54460
Parth Bhindi <45254591+parth2020@users.noreply.github.com>
2019-11-21 12:49:33 +0530

    Prog 6

---
+++ Swap.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Enter Value of A: ");
+            a = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Enter Value of B: ");
+            b = Convert.ToInt16(Console.ReadLine());
+
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+            Console.WriteLine("Value of A After Swap is "+ a + " And B After Sawp is " +b );
+
+            Console.ReadKey();
+        }
+    }
+}
