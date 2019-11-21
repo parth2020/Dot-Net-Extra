86d08447c231a767610b21e6667a1872e17b3bee
Parth Bhindi <45254591+parth2020@users.noreply.github.com>
2019-11-21 13:40:54 +0530

    Prog 13

---
+++ Fibonacci.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace extra13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m = 0, n = 1, Ans, i, number;
+            Console.WriteLine("Enter a Number : ");
+            number = int.Parse(Console.ReadLine());
+            Console.WriteLine(m + " " + n + " ");
+            for (i = 2; i < number; ++i)
+            {
+                Ans = m + n;
+                Console.WriteLine(Ans + "");
+                m = n;
+                n = Ans;
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
